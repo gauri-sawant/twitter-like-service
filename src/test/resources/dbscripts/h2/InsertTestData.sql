fc69f9698c0b611fc20835afeefd499866de583f
Gauri Sawant <gasaw@nets.eu>
2019-12-08 21:38:11 +0100

    Removed unnecessary code

--- src/test/resources/dbscripts/h2/InsertTestData.sql
+++
@@ -1,4 +0,0 @@
-  
-  insert into users values(1, "gasaw", "Gauri", "Sawant");
-  insert into users values(2, "sgore", "Sanket", "Gore");
-  insert into users values(3, "skulk", "Shardul", "Kulkarni");
