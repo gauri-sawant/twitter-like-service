a6df82742171886697c03c3d9796da746d192f02
Gauri Sawant <gasaw@nets.eu>
2019-12-08 21:24:33 +0100

    Added tests, and code package refactored

---
+++ src/main/java/com/twitter/TwitterApplication.java
@@ -0,0 +1,15 @@
+package com.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class TwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/twitter/persistence/TweetRepository.java
@@ -0,0 +1,17 @@
+package com.twitter.persistence;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.twitter.model.Tweet;
+
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+	List<Tweet> getTweets(@Param("userId") Long userId);
+
+	/*@Modifying
+	@Query("DELETE FROM Tweet t WHERE t.user.userId = ?1")
+	void deleteUserTweets(@Param("userId") Long userId);*/
+}
