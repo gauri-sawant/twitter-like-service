1b4de9dffb554669b6754b7296518e9f9153b93f
Gauri Sawant <gasaw@nets.eu>
2019-12-09 13:03:34 +0100

    Added Mock Tests and added getters for TweetReplies DTO

--- src/main/java/com/twitter/dto/TweetRepliesDTO.java
+++ src/main/java/com/twitter/dto/TweetRepliesDTO.java
@@ -12,46 +12,52 @@ import io.swagger.annotations.ApiModel;
  * @author gauri sawant
  *
  */
-@ApiModel(
-        description = "Representation of a Tweet and Replies"
-)
+@ApiModel(description = "Representation of a Tweet and Replies")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonDeserialize(builder = TweetRepliesDTO.Builder.class)
 public class TweetRepliesDTO {
-	
+
 	@JsonProperty(value = "tweet")
 	private final TweetDTO tweetDTO;
-	
+
 	@JsonProperty(value = "replies")
 	private final List<RepliesDTO> repliesDTOs;
-	
-    private TweetRepliesDTO(Builder builder) {
-        this.tweetDTO = builder.tweetDTO;
-        this.repliesDTOs = builder.repliesDTOs;
-    }
-	
+
+	private TweetRepliesDTO(Builder builder) {
+		this.tweetDTO = builder.tweetDTO;
+		this.repliesDTOs = builder.repliesDTOs;
+	}
+
+	public TweetDTO getTweetDTO() {
+		return tweetDTO;
+	}
+
+	public List<RepliesDTO> getRepliesDTOs() {
+		return repliesDTOs;
+	}
+
 	public static class Builder extends BuilderConstraints<TweetRepliesDTO> {
-		
+
 		@JsonProperty(value = "tweet")
 		private TweetDTO tweetDTO;
-		
+
 		@JsonProperty(value = "replies")
 		private List<RepliesDTO> repliesDTOs;
-		
+
 		public Builder tweetDTO(TweetDTO tweetDTO) {
-            this.tweetDTO = tweetDTO;
-            return this;
-        }
-        
-        public Builder repliesDTOs(List<RepliesDTO> repliesDTOs) {
-            this.repliesDTOs = repliesDTOs;
-            return this;
-        }
-        
-        @Override
-        public TweetRepliesDTO getTargetObject() {
-            return new TweetRepliesDTO(this);
-        }
+			this.tweetDTO = tweetDTO;
+			return this;
+		}
+
+		public Builder repliesDTOs(List<RepliesDTO> repliesDTOs) {
+			this.repliesDTOs = repliesDTOs;
+			return this;
+		}
+
+		@Override
+		public TweetRepliesDTO getTargetObject() {
+			return new TweetRepliesDTO(this);
+		}
 
 	}
 }
--- src/test/java/com/twitter/controller/rest/TweetRESTServiceImplTest.java
+++ src/test/java/com/twitter/controller/rest/TweetRESTServiceImplTest.java
@@ -2,6 +2,7 @@ package com.twitter.controller.rest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
@@ -9,8 +10,10 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.ws.rs.core.Response;
 
@@ -22,7 +25,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import com.twitter.dto.RepliesDTO;
 import com.twitter.dto.TweetDTO;
-import com.twitter.dto.UserDTO;
+import com.twitter.dto.TweetRepliesDTO;
 import com.twitter.model.Replies;
 import com.twitter.model.Tweet;
 import com.twitter.model.User;
@@ -79,7 +82,7 @@ public class TweetRESTServiceImplTest {
 		
 		User user = createTweetUser(1L, "userName", "firstName", "lastName");
 		Tweet tweet = createTweet(1L, "tweetText", "tweetfileName", 1L);
-		Replies reply = createReply(1L, "replyText", "replyfileName");
+		Replies reply = createReply(1L, "replyText", "replyfileName", 2L);
 		when(replyRepo.save(any(Replies.class))).thenReturn(reply);
 		when(userRESTServiceImpl.findUser(anyLong())).thenReturn(Optional.of(user));
 		when(tweetRepo.findById(anyLong())).thenReturn(Optional.of(tweet));
@@ -105,11 +108,93 @@ public class TweetRESTServiceImplTest {
 		assertThat(((List<TweetDTO>) response.getEntity()).size(), is(2));
 		assertThat(response.getStatus(), is(200));
 	}
+	
+	@Test
+	public void shouldNotGetTweetsForIncorrectUserId() {
+		Response response = tweetRESTServiceImpl.getTweetsForUser("PP");
+		assertThat(response.getStatus(), is(400));
+	}
+	
+	@Test
+	public void shouldGetTweetsAndFollowerRepliesForUser() {
+
+		Tweet tweet1 = createTweet(1L, "tweetText1", "tweetfileName1", 1L);
+		Replies replies = createReply(1L, "replyText", "replyfileName", 2L);
+
+		// Add Followers
+		User followerUser = new User();
+		followerUser.setUserId(2L);
+		followerUser.setUserName("userName2");
+
+		Set<User> userSet = new HashSet<>();
+		userSet.add(followerUser);
+
+		tweet1.getUser().setFollowerUser(userSet);
+		
+		//Add replies
+		List<Replies> replyList = new ArrayList<>();
+		replyList.add(replies);
+		tweet1.setReplies(replyList);
+
+		List<Tweet> tweetList = new ArrayList<>();
+		tweetList.add(tweet1);
+
+		when(tweetRepo.getTweets(anyLong())).thenReturn(tweetList);
+
+		Response response = tweetRESTServiceImpl.getFollowerTweetRepliesForUser("1");
+		assertThat(((List<TweetRepliesDTO>) response.getEntity()).size(), is(1));
+		assertThat(response.getStatus(), is(200));
+	}
+	
+	@Test
+	public void shouldGetTweetsAndNoRepliesForUserIfFollowersNotPresent() {
+
+		Tweet tweet1 = createTweet(1L, "tweetText1", "tweetfileName1", 1L);
+		Replies replies = createReply(1L, "replyText", "replyfileName", 2L);
+		//Add replies
+		List<Replies> replyList = new ArrayList<>();
+		replyList.add(replies);
+		tweet1.setReplies(replyList);
+
+		List<Tweet> tweetList = new ArrayList<>();
+		tweetList.add(tweet1);
+
+		when(tweetRepo.getTweets(anyLong())).thenReturn(tweetList);
+
+		Response response = tweetRESTServiceImpl.getFollowerTweetRepliesForUser("1");
+		List<TweetRepliesDTO> tweetRepliesDTO = (List<TweetRepliesDTO>) response.getEntity();
+		assertThat((tweetRepliesDTO).size(), is(1));
+		
+		List<RepliesDTO> repliesDTO = tweetRepliesDTO.get(0).getRepliesDTOs();
+		assertThat((repliesDTO).size(), is(0));
+		assertThat(response.getStatus(), is(200));
+	}
+	
+	
+	@Test
+	public void shouldNotGetTweetsAndRepliesForUserIfTweetsNotPresent() {
+
+		when(tweetRepo.getTweets(anyLong())).thenReturn(null);
+		Response response = tweetRESTServiceImpl.getFollowerTweetRepliesForUser("1");
+		List<TweetRepliesDTO> tweetRepliesDTO = (List<TweetRepliesDTO>) response.getEntity();
+		assertThat((tweetRepliesDTO), is(nullValue()));
+		assertThat(response.getStatus(), is(500));
+	}
+	
+	
+	@Test
+	public void shouldNotGetTweetsAndRepliesForIncorrectUserId() {
+
+		Response response = tweetRESTServiceImpl.getFollowerTweetRepliesForUser("PP");
+		List<TweetRepliesDTO> tweetRepliesDTO = (List<TweetRepliesDTO>) response.getEntity();
+		assertThat((tweetRepliesDTO), is(nullValue()));
+		assertThat(response.getStatus(), is(400));
+	}
+
 
 	private RepliesDTO createReplyDTO(String replyText, String replyfileName) {
 		return new RepliesDTO.Builder().replyText(replyText).replyAttachment(replyfileName).build();
 	}
-
 
 	private User createTweetUser(long id, String userName, String firstName, String lastName) {
 		User user = new User();
@@ -136,11 +221,15 @@ public class TweetRESTServiceImplTest {
 		return tweet;
 	}
 	
-	private Replies createReply(long tweetId, String tweetText, String tweetfileName) {
+	private Replies createReply(long tweetId, String replyText, String replyFileName, Long userId) {
 		Replies reply = new Replies();
 		reply.setReplyId(tweetId);
-		reply.setText(tweetText);
-		reply.setAttachmentFileName(tweetfileName);
+		reply.setText(replyText);
+		reply.setAttachmentFileName(replyFileName);
+		
+		User user = new User(userId);
+		user.setUserName("userName");
+		reply.setUser(user);
 		return reply;
 	}
 
