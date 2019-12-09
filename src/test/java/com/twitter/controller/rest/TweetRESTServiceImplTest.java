674c130300b3425f0b9c70b57f6118a5db4032ad
Gauri Sawant <gasaw@nets.eu>
2019-12-09 11:47:21 +0100

    Added Mock tests

--- src/test/java/com/twitter/controller/rest/TweetRESTServiceImplTest.java
+++ src/test/java/com/twitter/controller/rest/TweetRESTServiceImplTest.java
@@ -5,7 +5,11 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javax.ws.rs.core.Response;
@@ -18,6 +22,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import com.twitter.dto.RepliesDTO;
 import com.twitter.dto.TweetDTO;
+import com.twitter.dto.UserDTO;
 import com.twitter.model.Replies;
 import com.twitter.model.Tweet;
 import com.twitter.model.User;
@@ -48,7 +53,7 @@ public class TweetRESTServiceImplTest {
 	public void shouldCreateNewTweet() {
 		
 		User user = createTweetUser(1L, "userName", "firstName", "lastName");
-		Tweet tweet = createTweet(1L, "tweetText", "tweetfileName");
+		Tweet tweet = createTweet(1L, "tweetText", "tweetfileName",1L);
 		when(tweetRepo.save(any(Tweet.class))).thenReturn(tweet);
 		when(userRESTServiceImpl.findUser(anyLong())).thenReturn(Optional.of(user));
 		TweetDTO tweetDTO = createTweetDTO("tweetText", "tweetfileName");
@@ -57,12 +62,23 @@ public class TweetRESTServiceImplTest {
 		assertThat(response.getStatus(), is(201));
 	}
 	
+	
+	@Test
+	public void shouldNotCreateTweetWhenUserNotFound() {
+		
+		when(userRESTServiceImpl.findUser(anyLong())).thenReturn(null);
+		TweetDTO tweetDTO = createTweetDTO("tweetText", "tweetfileName");
+		Response response = tweetRESTServiceImpl.createTweet(tweetDTO, "1");
+		verify(tweetRepo, never()).save(any(Tweet.class));
+		assertThat(response.getStatus(), is(500));
+	}
+	
 
 	@Test
 	public void shouldAddNewReply() {
 		
 		User user = createTweetUser(1L, "userName", "firstName", "lastName");
-		Tweet tweet = createTweet(1L, "tweetText", "tweetfileName");
+		Tweet tweet = createTweet(1L, "tweetText", "tweetfileName", 1L);
 		Replies reply = createReply(1L, "replyText", "replyfileName");
 		when(replyRepo.save(any(Replies.class))).thenReturn(reply);
 		when(userRESTServiceImpl.findUser(anyLong())).thenReturn(Optional.of(user));
@@ -71,6 +87,23 @@ public class TweetRESTServiceImplTest {
 		Response response = tweetRESTServiceImpl.addReply(replyDTO, "1", "1");
 		verify(replyRepo).save(any(Replies.class));
 		assertThat(response.getStatus(), is(201));
+	}
+	
+	
+	@Test
+	public void shouldGetTweetsForUser() {
+		
+		Tweet tweet1 = createTweet(1L, "tweetText1", "tweetfileName1", 1L);
+		Tweet tweet2 = createTweet(2L, "tweetText2", "tweetfileName2", 1L);
+		
+		List<Tweet> tweetList = new ArrayList<>(); 
+		tweetList.add(tweet1);
+		tweetList.add(tweet2);
+		when(tweetRepo.getTweets(anyLong())).thenReturn(tweetList);
+		
+		Response response = tweetRESTServiceImpl.getTweetsForUser("1");
+		assertThat(((List<TweetDTO>) response.getEntity()).size(), is(2));
+		assertThat(response.getStatus(), is(200));
 	}
 
 	private RepliesDTO createReplyDTO(String replyText, String replyfileName) {
@@ -91,11 +124,15 @@ public class TweetRESTServiceImplTest {
 		return new TweetDTO.Builder().tweetText(tweetText).tweetAttachment(tweetfileName).build();
 	}
 
-	private Tweet createTweet(long tweetId, String tweetText, String tweetfileName) {
+	private Tweet createTweet(long tweetId, String tweetText, String tweetfileName, Long userId) {
 		Tweet tweet = new Tweet();
 		tweet.setTweetId(tweetId);
 		tweet.setText(tweetText);
 		tweet.setAttachmentFileName(tweetfileName);
+		
+		User user = new User(userId);
+		user.setUserName("userName");
+		tweet.setUser(user);
 		return tweet;
 	}
 	
